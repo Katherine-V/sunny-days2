8343c77cef9ba2ae21ebde587df17c60a1530874
Katherine V <msvogler919@gmail.com>
2024-01-19 20:30:40 -0500

    trying to undo nesting repos

---
+++ README.md
@@ -0,0 +1,8 @@
+# sunny-days
+# purpose 
+Allows for weather forecasts searchable by location, making use of an API
+
+work in progress
+
+#credits
+made use of tutorial by "Asish George Tech" available at (website goes here)
---
+++ scripts/script.js
@@ -0,0 +1,34 @@
+const timeE1=document.getElementById('time');
+const dateE1= document.getElementById('date')
+const currentWeatherItemsE1= document.getElementById('current-weather-items');
+const timezone= document.getElementById('time-zone');
+const countryE1= document.getElementById('country');
+const weatherforecastE1= document.getElementById('weather-forecast');
+const currentTempE1= document.getElementById('current-temp');
+
+const days=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday','Saturday'];
+const months=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const API_KEY="de9febdd9b2cf4fbef220c7bcee43367";
+
+setInterval(()=>{
+    const time = newDate();
+    const month = time.getMonth();
+    const date = time.getDate();
+    const day = time.getDay();
+    const hour = time.getHours();
+    const hoursIn12HrFormat = hour >=13 ? hour%12:hour
+    const minutes = time.getMinutes();
+    const ampm = hour>=12 ? "pm" : "am"
+}, 1000);
+
+timeE1.innerHTML = hoursIn12HrFormat+ ':' +minutes+ ''+`<span id='am-pm'>${ampm}</span>`
+
+dateE1.innerHTML = days[day]+', '+date+''+months[month]
+
+function getWeatherData(){
+    navigator.geolocation.getCurrentPosition((success => {
+    console.log(success);
+        let{latitude, longitude} = success.coords;
+
+    }))
